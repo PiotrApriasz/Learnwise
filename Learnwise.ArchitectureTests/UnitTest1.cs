331fba8d10b9fcde697666681bb96b1b213ea567
PiotrApriasz <piotrekapriasz@gmail.com>
2024-08-22 00:50:16 +0200

    Initialize repository

---
+++ Learnwise.ArchitectureTests/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace Learnwise.ArchitectureTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
---
+++ Learnwise.UnitTests/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace Learnwise.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
